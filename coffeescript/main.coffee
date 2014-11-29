15fc00b48046d68d194401648380ab684647da42
Brian Hall <brian@reallymany.com>
2014-11-29 13:03:25 -1000

    file system drawn on screen, i am so proud

--- coffeescript/main.coffee
+++ coffeescript/main.coffee
@@ -21,12 +21,41 @@ jQuery ->
 
 		## DRAW FILE SYSTEM MAP ##
 		#
-		text = new createjs.Text("/", "20px Arial", "black")
-		text.x = 250
-		text.y = 20
-		text.textBaseline = "alphabetic"
-		stage.addChild(text)
+		# Write text for available folders
+		rootText = new createjs.Text("/", "20px Arial", "black")
+		rootText.x = 250
+		rootText.y = 120
+		rootText.textBaseline = "alphabetic"
+		stage.addChild(rootText)
 
+		homeText = new createjs.Text("home", "20px Arial", "black")
+		homeText.x = 140
+		homeText.y = 235
+		homeText.textBaseline = "alphabetic"
+		stage.addChild(homeText)
+
+		mediaText = new createjs.Text("media", "20px Arial", "black")
+		mediaText.x = 340
+		mediaText.y = 235
+		mediaText.textBaseline = "alphabetic"
+		stage.addChild(mediaText)
+
+		# Draw lines from root to children
+		line1 = new createjs.Shape()
+		line1.graphics.setStrokeStyle(1)
+		line1.graphics.beginStroke("gray")
+		line1.graphics.moveTo(255, 125)
+		line1.graphics.lineTo(350, 220)
+		line1.graphics.endStroke()
+		stage.addChild(line1)
+
+		line2 = new createjs.Shape()
+		line2.graphics.setStrokeStyle(1)
+		line2.graphics.beginStroke("gray")
+		line2.graphics.moveTo(245, 125)
+		line2.graphics.lineTo(150, 220)
+		line2.graphics.endStroke()
+		stage.addChild(line2)
 
 		## CREATE CHARACTER SPRITE ##
 		bashySpriteSheet = new createjs.SpriteSheet({
--- javascript/bashy.js
+++ javascript/bashy.js
@@ -93,12 +93,36 @@
       return stage.update();
     };
     return startGame = function() {
-      var bashySpriteSheet, bashy_sprite, os, sprite, text;
-      text = new createjs.Text("/", "20px Arial", "black");
-      text.x = 250;
-      text.y = 20;
-      text.textBaseline = "alphabetic";
-      stage.addChild(text);
+      var bashySpriteSheet, bashy_sprite, homeText, line1, line2, mediaText, os, rootText, sprite;
+      rootText = new createjs.Text("/", "20px Arial", "black");
+      rootText.x = 250;
+      rootText.y = 120;
+      rootText.textBaseline = "alphabetic";
+      stage.addChild(rootText);
+      homeText = new createjs.Text("home", "20px Arial", "black");
+      homeText.x = 140;
+      homeText.y = 235;
+      homeText.textBaseline = "alphabetic";
+      stage.addChild(homeText);
+      mediaText = new createjs.Text("media", "20px Arial", "black");
+      mediaText.x = 340;
+      mediaText.y = 235;
+      mediaText.textBaseline = "alphabetic";
+      stage.addChild(mediaText);
+      line1 = new createjs.Shape();
+      line1.graphics.setStrokeStyle(1);
+      line1.graphics.beginStroke("gray");
+      line1.graphics.moveTo(255, 125);
+      line1.graphics.lineTo(350, 220);
+      line1.graphics.endStroke();
+      stage.addChild(line1);
+      line2 = new createjs.Shape();
+      line2.graphics.setStrokeStyle(1);
+      line2.graphics.beginStroke("gray");
+      line2.graphics.moveTo(245, 125);
+      line2.graphics.lineTo(150, 220);
+      line2.graphics.endStroke();
+      stage.addChild(line2);
       bashySpriteSheet = new createjs.SpriteSheet({
         images: [bashy_himself],
         frames: {
